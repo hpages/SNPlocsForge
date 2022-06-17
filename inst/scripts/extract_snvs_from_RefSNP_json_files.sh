ab0ae895e4848e650a972a2dbd595c9cd415237f
Hervé Pagès <hpages.on.github@gmail.com>
2022-06-16 20:46:47 -0400

    small tweak

--- inst/scripts/extract_snvs_from_RefSNP_json_files.sh
+++ inst/scripts/extract_snvs_from_RefSNP_json_files.sh
@@ -5,7 +5,7 @@ set -e  # exit immediately if a simple command returns a non-zero status
 
 # Settings for dbSNP155 on rex3 (80 logical cpus):
 JSON_DIR="/home/hpages/SNPlocsForge/downloads/dbSNP155"
-DUMP_DIR="$JSON_DIR/snvs_dump2"
+DUMP_DIR="$JSON_DIR/snvs_dump"
 JSON_PREFIX="refsnp-"
 JSON_SUFFIX=".json.bz2"
 Rscript="/home/hpages/R/R-4.2.0/bin/Rscript"
@@ -36,12 +36,12 @@ start_extraction chr5  5 11000 &
 start_extraction chr6  5 11000 &
 start_extraction chr7  5 11000 &
 start_extraction chr8  5 11000 &
-start_extraction chr9  4 10000 &
-start_extraction chr10 4 10000 &
-start_extraction chr11 4 10000 &
-start_extraction chr12 4 10000 &
-start_extraction chr13 4 10000 &
-start_extraction chr14 4 10000 &
+start_extraction chr9  5 11000 &
+start_extraction chr10 1  5000 &
+start_extraction chr11 1  5000 &
+start_extraction chr12 1  5000 &
+start_extraction chr13 1  5000 &
+start_extraction chr14 1  5000 &
 start_extraction chr15 1  5000 &
 start_extraction chr16 1  5000 &
 start_extraction chr17 1  5000 &
@@ -50,7 +50,7 @@ start_extraction chr19 1  5000 &
 start_extraction chr20 1  5000 &
 start_extraction chr21 1  5000 &
 start_extraction chr22 1  5000 &
-start_extraction chrX  4 10000 &
+start_extraction chrX  1  5000 &
 start_extraction chrY  1  5000 &
 start_extraction chrMT 1  5000 &
 
