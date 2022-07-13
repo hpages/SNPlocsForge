4f7c1444aea1d233ed85cf3c003dffeb0cceaae9
Hervé Pagès <hpages.on.github@gmail.com>
2022-07-13 17:57:09 -0400

    Add select_GRCh38_snvs.sh script

--- inst/scripts/dbSNP155/select_GRCh38_snvs.sh
+++ inst/scripts/dbSNP155/select_GRCh38_snvs.sh
@@ -2,15 +2,15 @@
 #
 # To run this script in "batch mode":
 #
-#   /home/hpages/SNPlocsForge/SNPlocsForge/inst/scripts/dbSNP155/select_GRCh37_snvs.sh >select_GRCh37_snvs.log 2>&1 &
+#   /home/hpages/SNPlocsForge/SNPlocsForge/inst/scripts/dbSNP155/select_GRCh38_snvs.sh >select_GRCh38_snvs.log 2>&1 &
 #
 
 set -e  # Exit immediately if a simple command exits with a non-zero status
 
 ## Settings for rex3:
-ASSEMBLY="GRCh37.p13"
+ASSEMBLY="GRCh38.p13"
 DUMP_DIR="/home/hpages/SNPlocsForge/downloads/dbSNP155/snvs_dump"
-OUT_DIR="/home/hpages/SNPlocsForge/downloads/dbSNP155/GRCh37_snvs"
+OUT_DIR="/home/hpages/SNPlocsForge/downloads/dbSNP155/GRCh38_snvs"
 Rscript="/home/hpages/R/R-4.2.0/bin/Rscript"
 
 SEQNAMES="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y MT"
